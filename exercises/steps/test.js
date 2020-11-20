463b79e94002681da9cf66dcc1fac89ec3a73406
extasy44 <seoheejun@gmail.com>
2020-11-21 08:30:56 +1100

    exercise steps, pyramid

--- exercises/steps/test.js
+++ exercises/steps/test.js
@@ -32,3 +32,12 @@ test('steps called with n = 3', () => {
   expect(console.log.mock.calls[2][0]).toEqual('###');
   expect(console.log.mock.calls.length).toEqual(3);
 });
+
+test('steps called with n = 4', () => {
+  steps(4);
+  expect(console.log.mock.calls[0][0]).toEqual('#   ');
+  expect(console.log.mock.calls[1][0]).toEqual('##  ');
+  expect(console.log.mock.calls[2][0]).toEqual('### ');
+  expect(console.log.mock.calls[3][0]).toEqual('####');
+  expect(console.log.mock.calls.length).toEqual(4);
+});
